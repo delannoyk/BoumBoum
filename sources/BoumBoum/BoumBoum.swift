09fbb30af3b76109d2847149acd835b483d2bae2
Kevin Delannoy <delannoyk@gmail.com>
2016-02-08 07:51:41 +0100

    Added documentation

--- sources/BoumBoum/BoumBoum.swift
+++ sources/BoumBoum/BoumBoum.swift
@@ -9,6 +9,16 @@
 import UIKit
 import AVFoundation
 
+/**
+ Possible BoumBoum Errors.
+
+ - AlreadyRunning:                  Means you asked to start monitoring while it already is.
+ - AlreadyStopped:                  Means you asked to stop monitoring while it's not.
+ - CameraUnavailable:               Means the camera isn't available.
+ - TorchModeUnsupported:            Means the torch isn't available - though needed.
+ - CameraLockFailed:                Means the camera lock failed to update the configuration.
+ - CameraDeviceInputCreationFailed: Means the camera device input creation failed.
+ */
 public enum BoumBoumError: ErrorType {
     case AlreadyRunning
     case AlreadyStopped
@@ -18,40 +28,91 @@ public enum BoumBoumError: ErrorType {
     case CameraDeviceInputCreationFailed(NSError)
 }
 
+/// The delegate protocol.
 public protocol BoumBoumDelegate: class {
+    /**
+     Called when the averate heart rate is updated. At start, it may take a while
+     but after the first average has been computed, it will be called regularly.
+
+     - parameter boumBoum: BoumBoum instance.
+     - parameter rate:     The heart rate.
+     */
     func boumBoum(boumBoum: BoumBoum, didFindAverageRate rate: UInt)
 }
 
+/// BoumBoum is an heart rate monitor that use value from the camera.
+/// Place your finger on the back camera with a part of your finger on
+/// the torch and see your heart rate.
 public class BoumBoum: NSObject {
+    /**
+     Possible BoumBoum states.
+
+     - Running: BoumBoum is monitoring.
+     - Stopped: BoumBoum isn't monitoring.
+     */
     public enum State {
         case Running
         case Stopped
     }
 
+    /// The capture session.
     public let session: AVCaptureSession
+
+    /// The camera.
     public let camera: AVCaptureDevice?
+
+    /// The delegate.
     public weak var delegate: BoumBoumDelegate?
 
+
+    /// The state.
     public private(set) var state = State.Stopped
 
+
+    /// The rate analyzer.
     private let analyzer = BoumBoumAnalyzer()
+
+    /// The camera input.
     private var cameraInput: AVCaptureDeviceInput?
+
+    /// The video output.
     private var videoOutput: AVCaptureVideoDataOutput?
 
     // MARK: Initialization
     ////////////////////////////////////////////////////////////////////////////
 
-    public override convenience init() {
+    /**
+    Initializes BoumBoum with a new `AVCaptureSession` and the default camera.
+
+    - returns: An initialized BoumBoum.
+    */
+    public convenience override init() {
         self.init(session: AVCaptureSession(),
             camera: AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo))
     }
 
+    /**
+     Initializes BoumBoum with a given `AVCaptureSession` and a given camera.
+
+     - parameter session: A capture session.
+     - parameter camera:  A reference to the back camera.
+
+     - returns: An initialized BoumBoum.
+     */
     public init(session: AVCaptureSession, camera: AVCaptureDevice) {
         self.session = session
         self.camera = camera
         super.init()
     }
 
+    /**
+     Initializes BoumBoum with a given `AVCaptureSession` and a given camera.
+
+     - parameter session: A capture session.
+     - parameter camera:  A reference to the back camera.
+
+     - returns: An initialized BoumBoum.
+     */
     private init(session: AVCaptureSession, camera: AVCaptureDevice?) {
         self.session = session
         self.camera = camera
@@ -64,6 +125,11 @@ public class BoumBoum: NSObject {
     // MARK: Start / Stop
     ////////////////////////////////////////////////////////////////////////////
 
+    /**
+    Starts monitoring the heart rate.
+
+    - throws: A `BoumBoumError` if something fails.
+    */
     public func startMonitoring() throws {
         //Ensuring we have a camera
         guard let camera = camera else {
@@ -116,6 +182,11 @@ public class BoumBoum: NSObject {
         state = .Running
     }
 
+    /**
+     Stops monitoring the heart rate.
+
+     - throws: A `BoumBoumError` if something fails.
+     */
     public func stopMonitoring() throws {
         //Ensuring we have a camera
         guard let camera = camera else {
@@ -161,6 +232,13 @@ public class BoumBoum: NSObject {
     // MARK: Output creation
     ////////////////////////////////////////////////////////////////////////////
 
+    /**
+    Configures an `AVCatpureVideoDataOutput`.
+
+    - parameter delegate: The delegate to set.
+
+    - returns: A configured `AVCatpureVideoDataOutput`.
+    */
     private func configuredCameraOutput(delegate: AVCaptureVideoDataOutputSampleBufferDelegate) -> AVCaptureVideoDataOutput {
         let videoOutput = AVCaptureVideoDataOutput()
         let queue = dispatch_queue_create("be.delannoyk.boumboum", nil)
